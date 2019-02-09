f76962be6c3962a1e69a5ef512a7419f83a110c4
M <misko1099@hotmail.com>
2019-02-09 18:08:56 -0500

    L04
    L04-MusicStore and Enrollment applications

---
+++ Labs/L04/MVCEnrollmentApplication/MVCEnrollmentApplication/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace MVCEnrollmentApplication
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ Labs/L04/MVCEnrollmentApplication/MVCEnrollmentApplication/Models/Student.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCEnrollmentApplication.Models
+{
+    public class Student
+    {
+        public virtual int StudentID { get; set; }
+        public virtual string FirstName { get; set; }
+        public virtual string LastName { get; set; }
+
+    }
+}
