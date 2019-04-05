6a4df7d76f9db12b484d4eb289de8260129dc2bb
M <misko1099@hotmail.com>
2019-04-05 02:26:23 -0400

    L11
    Lab 11 - Third submission

--- Labs/L11/MVCMusicStoreApplication/MVCMusicStoreApplication/Scripts/Apps/MusicScripts.js
+++ Labs/L11/MVCMusicStoreApplication/MVCMusicStoreApplication/Scripts/Apps/MusicScripts.js
@@ -5,7 +5,7 @@
         $.post("/ShoppingCart/RemoveFromCart", { "id": id }, function (data) {
             $("#update-message").text(data.Message);
             $("#cart-total").text(data.CartTotal);
-            $("#item-count" + data.DeleteId).text(data.ItemCount);
+            $("#item-count-" + data.DeleteId).text(data.ItemCount);
 
             if (data.ItemCount < 1) {
                 $("#record-" + data.DeleteId).fadeOut();
