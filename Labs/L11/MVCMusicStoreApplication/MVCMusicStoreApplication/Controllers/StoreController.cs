05e3c4f19181f6d3a307acb0daf3da92038aa73f
M <misko1099@hotmail.com>
2019-04-05 01:57:20 -0400

    L10
    Lab 10 - First submission

---
+++ Labs/L11/MVCMusicStoreApplication/MVCMusicStoreApplication/Controllers/StoreController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVCMusicStoreApplication.Models;
+
+namespace MVCMusicStoreApplication.Controllers
+{
+    public class StoreController : Controller
+    {
+        private MVCMusicStoreDB db = new MVCMusicStoreDB();
+
+        public ActionResult Browse()
+        {
+            return View(db.Genres.OrderBy(a => a.Name).ToList());
+        }
+
+        
+        public ActionResult Index(int id)
+        {
+            var albums = GetAlbums(id);
+
+            return View("Index", albums);
+        }
+
+        private List<Album> GetAlbums(int searchInt)
+        {
+                return db.Albums
+                .Where(a => a.GenreId.Equals(searchInt))
+                .OrderBy(a => a.Title)
+                .ToList();
+        }
+
+        public ActionResult Details(int id)
+        {
+            Album album = db.Albums.Find(id);
+
+            return View("Details", album);
+        }
+    }
+}
---
+++ Labs/L11/MVCMusicStoreApplication/MVCMusicStoreApplication/Scripts/Apps/MusicScripts.js
@@ -0,0 +1,16 @@
+﻿$(function () {
+    $(".RemoveLink").click(function () {
+        var id = $(this).attr("data-id");
+
+        $.post("/ShoppingCart/RemoveFromCart", { "id": id }, function (data) {
+            $("#update-message").text(data.Message);
+            $("#cart-total").text(data.CartTotal);
+            $("#item-count" + data.DeletedId).text(data.ItemCount);
+            
+
+            if (data.ItemCount < 1) {
+                $("#record-" + data.DeleteId).fadeOut();
+            }
+        });
+    });
+});
