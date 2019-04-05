0893dcd74940fb77bdd713984a6bdb91bd9c366b
M <misko1099@hotmail.com>
2019-04-05 02:21:49 -0400

    L11
    Lab 11 - Second submission

--- Labs/L11/MVCMusicStoreApplication/MVCMusicStoreApplication/Scripts/Apps/MusicScripts.js
+++ Labs/L11/MVCMusicStoreApplication/MVCMusicStoreApplication/Scripts/Apps/MusicScripts.js
@@ -5,12 +5,11 @@
         $.post("/ShoppingCart/RemoveFromCart", { "id": id }, function (data) {
             $("#update-message").text(data.Message);
             $("#cart-total").text(data.CartTotal);
-            $("#item-count" + data.DeletedId).text(data.ItemCount);
-            
+            $("#item-count" + data.DeleteId).text(data.ItemCount);
 
             if (data.ItemCount < 1) {
                 $("#record-" + data.DeleteId).fadeOut();
             }
         });
-    });
+    })
 });
