8c25f3780c64a2eef0962ba15c104e9497ebddae
M <misko1099@hotmail.com>
2019-04-23 21:53:30 -0400

    FinalProject
    FinalProject - Test submission

---
+++ Labs/FinalProject/EventApplication/EventApplication/Models/EventType.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EventApplication.Models
+{
+    public class EventType
+    {
+        public virtual int EventTypeId { get; set; }
+        public virtual string Type { get; set; }
+    }
+}
